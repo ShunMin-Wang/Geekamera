f8a9c00894b190b67e7da7a557b62f89326c878d
josh-hsu <alenbos0517@gmail.com>
2016-05-30 17:35:28 +0800

    geekamera: add device name and peer discovery

---
+++ app/src/main/java/com/gg/geekamera/GeekBroadcastReceiver.java
@@ -0,0 +1,124 @@
+package com.gg.geekamera;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.NetworkInfo;
+import android.net.wifi.WpsInfo;
+import android.net.wifi.p2p.WifiP2pConfig;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pDeviceList;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GeekBroadcastReceiver extends BroadcastReceiver {
+    private final static String TAG = "Geekamera";
+    private WifiP2pManager mManager;
+    private WifiP2pManager.Channel mChannel;
+    private MainActivity activity;
+    private List peers = new ArrayList();
+
+    public GeekBroadcastReceiver(WifiP2pManager manager, WifiP2pManager.Channel channel, MainActivity activity) {
+        super();
+        this.mManager = manager;
+        this.mChannel = channel;
+        this.activity = activity;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+        if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+            // Determine if Wifi P2P mode is enabled or not, alert
+            // the Activity.
+            int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
+            if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
+                //activity.setIsWifiP2pEnabled(true);
+                Log.d(TAG, "Wifi is enabled");
+            } else {
+                Log.d(TAG, "Wifi is disabled");
+            }
+        } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+
+            if (mManager != null) {
+                mManager.requestPeers(mChannel, peerListListener);
+            }
+            Log.d(TAG, "P2P peers changed");
+
+        } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
+
+            // Connection state changed!  We should probably do something about
+            // that.
+
+            if (mManager == null) {
+                return;
+            }
+
+            NetworkInfo networkInfo = (NetworkInfo) intent
+                    .getParcelableExtra(WifiP2pManager.EXTRA_NETWORK_INFO);
+
+            if (networkInfo.isConnected()) {
+
+                // We are connected with the other device, request connection
+                // info to find group owner IP
+
+                //mManager.requestConnectionInfo(mChannel, connectionListener);
+            }
+
+        } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
+            WifiP2pDevice device = intent.getParcelableExtra(WifiP2pManager.EXTRA_WIFI_P2P_DEVICE);
+            activity.updateSelfId(device.deviceName);
+        }
+    }
+
+    private WifiP2pManager.PeerListListener peerListListener = new WifiP2pManager.PeerListListener() {
+        @Override
+        public void onPeersAvailable(WifiP2pDeviceList peerList) {
+
+            // Out with the old, in with the new.
+            peers.clear();
+            peers.addAll(peerList.getDeviceList());
+
+            // If an AdapterView is backed by this data, notify it
+            // of the change.  For instance, if you have a ListView of available
+            // peers, trigger an update.
+            //((WiFiPeerListAdapter) getListAdapter()).notifyDataSetChanged();
+            if (peers.size() == 0) {
+                Log.d("", "No devices found");
+                return;
+            } else {
+                Log.d(TAG, peers.toString());
+                activity.updatePeerList(peers);
+                //connect();
+            }
+        }
+    };
+
+    public void connect() {
+        // Picking the first device found on the network.
+        WifiP2pDevice device = (WifiP2pDevice)peers.get(0);
+
+        WifiP2pConfig config = new WifiP2pConfig();
+        config.deviceAddress = device.deviceAddress;
+        config.wps.setup = WpsInfo.PBC;
+
+        mManager.connect(mChannel, config, new WifiP2pManager.ActionListener() {
+
+            @Override
+            public void onSuccess() {
+                // WiFiDirectBroadcastReceiver will notify us. Ignore for now.
+            }
+
+            @Override
+            public void onFailure(int reason) {
+                Log.d(TAG, "connect failed retry");
+            }
+        });
+    }
+
+
+}
+
--- app/src/main/java/com/gg/geekamera/MainActivity.java
+++ app/src/main/java/com/gg/geekamera/MainActivity.java
@@ -1,18 +1,133 @@
 package com.gg.geekamera;
 
+import android.content.Context;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "GG";
+    private final static String TAG = "Geekamera";
+
+    /* UI */
+    private String[] list = {"No peer, press refresh to re-discover"};
+    public ArrayAdapter<String> mAdapter;
+    public ListView mPeerListView;
+    public TextView mSelfIdTextView;
+
+
+    /* WiFi P2P */
+    WifiP2pManager.Channel mChannel;
+    WifiP2pManager mManager;
+    GeekBroadcastReceiver receiver;
+    private IntentFilter intentFilter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d(TAG, "Git upload test");
+        mPeerListView = (ListView) findViewById(R.id.listViewPeerList);
+        mAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, list);
+        mPeerListView.setAdapter(mAdapter);
+        mPeerListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), "WTF..you hit list", Toast.LENGTH_SHORT).show();
+            }
+        });
 
+        mSelfIdTextView = (TextView)findViewById(R.id.textViewMyId);
+
+        Button buttonDiscover = (Button) findViewById(R.id.buttonRefresh);
+        if (buttonDiscover != null) {
+            buttonDiscover.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    startDiscovering();
+                }
+            });
+        }
+
+        Button buttonVisible = (Button) findViewById(R.id.buttonBroadcast);
+        if (buttonVisible != null) {
+            buttonVisible.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                }
+            });
+        }
+        intentFilter =  new IntentFilter();
+        //  Indicates a change in the Wi-Fi P2P status.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+
+        // Indicates a change in the list of available peers.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+
+        // Indicates the state of Wi-Fi P2P connectivity has changed.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+
+        // Indicates this device's details have changed.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+        mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        mChannel = mManager.initialize(this, getMainLooper(), null);
     }
+
+    /** register the BroadcastReceiver with the intent values to be matched */
+    @Override
+    public void onResume() {
+        super.onResume();
+        receiver = new GeekBroadcastReceiver(mManager, mChannel, this);
+        registerReceiver(receiver, intentFilter);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        unregisterReceiver(receiver);
+    }
+
+    public void startDiscovering() {
+        mManager.discoverPeers(mChannel, new WifiP2pManager.ActionListener() {
+
+            @Override
+            public void onSuccess() {
+                // Code for when the discovery initiation is successful goes here.
+                // No services have actually been discovered yet, so this method
+                // can often be left blank.  Code for peer discovery goes in the
+                // onReceive method, detailed below.
+                Log.d(TAG, "discoverPeers success");
+            }
+
+            @Override
+            public void onFailure(int reasonCode) {
+                // Code for when the discovery initiation fails goes here.
+                // Alert the user that something went wrong.
+                Log.d(TAG, "discoverPeers failed");
+            }
+        });
+    }
+
+    public void updatePeerList(List<String> clist) {
+        mAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, clist);
+        mPeerListView.setAdapter(mAdapter);
+    }
+
+    public void updateSelfId(String id) {
+        mSelfIdTextView.setText(getText(R.string.main_my_id) + "  " + id);
+    }
+
 }
